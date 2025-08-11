0d8b92bd969d947ae1a6426779b579c76db91d89
Christian Delapos <172143881+NoirTempest@users.noreply.github.com>
2025-08-11 10:51:44 +0800

    sss

--- resources/views/livewire/approver/limited-access.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="container mt-4">
-    <div class="alert alert-warning">
-        <h4 class="alert-heading">Limited Access</h4>
-        <p>You are marked as an approver but do not have Level-1 approval privileges.</p>
-        <hr>
-        <a href="{{ route('dashboard') }}" class="btn btn-primary">Back to Dashboard</a>
-    </div>
-</div>
--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\CalendarEventController;
-
-Route::get('/calendar-events', [CalendarEventController::class, 'index']);
-
